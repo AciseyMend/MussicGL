83aba790dda5cc99160f9f05208987c18a9150f4
Lisset100 <liset10nieve@live.com.mx>
2023-06-16 11:34:03 -0600

    cambios

--- JS/app.js
+++ JS/app.js
@@ -2,17 +2,19 @@ console.log("funcion")
 console.log(document)
 console.log(document.body)
 console.log(document.getElementsByClassName("title"))
+const $guardarConfiguracion = document.getElementById("guardarConfiguracion");
+$guardarConfiguracion.addEventListener('click', guardarDatos);
 
 const $config__option = document.getElementsByClassName("config__option")
 const $config__button = document.getElementsByClassName("config__button")
 $config__button.onclick = Text
 
 
-function guardar_datos (event){
+function guardarDatos (event){
     event.preventDefault()
-    const $config__button = document.querySelector ("#config__button").value
+    const $guardarConfiguracion = document.querySelector ("#guardarConfiguracion").value
     
-console.log("Guardar configuración", $config__button)
+console.log("Guardar configuración", $guardarConfiguracion)
 
 alert("Se han guardado los cambios")
 }
